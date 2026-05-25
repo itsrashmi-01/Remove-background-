0bebb699c47ffe95846209dfd89c535c630c932a
I'TS RASHMI <itsrashmi4@gmail.com>
2026-05-25 22:02:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,18 @@
 
+from pyrogram import filters
+
+from server.client import app
+
+
+@app.on_message(filters.command("start"))
+async def start_handler(client, message):
+
+    text = """
+🌌 Welcome to Creator Cut AI
+
+✂️ Production-grade background remover.
+
+Send any image to remove background.
+"""
+
+    await message.reply_text(text)
