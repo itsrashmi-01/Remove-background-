2eb390244d5090df9f79eec0249219000157771e
I'TS RASHMI <itsrashmi4@gmail.com>
2026-05-25 22:45:10 +0530

    Update client.py

--- server/client.py
+++ server/client.py
@@ -3,9 +3,10 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
-    "RMBG_BOT",
+    "rmbg_bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    workers=50
+    workers=20,
+    sleep_threshold=30
 )
