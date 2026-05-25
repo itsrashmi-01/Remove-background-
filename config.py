7ed0ebcf441f8b3d3151cb8ee83db3350e4435b3
I'TS RASHMI <itsrashmi4@gmail.com>
2026-05-25 20:41:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+ADMIN_ID = int(os.getenv("OWNER_ID", "0"))
 
 MAX_IMAGE_SIZE = 10 * 1024 * 1024
 
