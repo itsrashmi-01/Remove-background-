62dd8f09e8749ff402049031961e402d982b4624
I'TS RASHMI <itsrashmi4@gmail.com>
2026-05-25 22:00:21 +0530

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,9 +1,11 @@
 import os
 import uuid
 
+
 def generate_filename(extension="png"):
     return f"temp/{uuid.uuid4().hex}.{extension}"
 
+
 def cleanup_file(path):
-    if os.path.exists(path):
+    if path and os.path.exists(path):
         os.remove(path)
