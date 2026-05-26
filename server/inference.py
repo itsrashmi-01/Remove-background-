355c1b08a019197bdbb0b1c6129254bb3d309cab
I'TS RASHMI <itsrashmi4@gmail.com>
2026-05-26 08:35:36 +0530

    Update inference.py

--- server/inference.py
+++ server/inference.py
@@ -1,57 +1,23 @@
-import torch
-import numpy as np
-
-from PIL import Image
-
-from transformers import AutoModelForImageSegmentation
-import torchvision.transforms as transforms
-
-from config import RMBG_MODEL_NAME, HF_TOKEN
+from rembg import remove
 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-print("📦 Loading RMBG-2.0 Model...")
-
-model = AutoModelForImageSegmentation.from_pretrained(
-    RMBG_MODEL_NAME,
-    trust_remote_code=True,
-    token=HF_TOKEN
-)
-
-model.to(device)
-model.eval()
-
-print("✅ RMBG-2.0 Loaded")
-
-
-transform_image = transforms.Compose([
-    transforms.Resize((1024, 1024)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5], [0.5])
-])
+print("📦 Loading Rembg Model...")
 
 
 async def remove_background(input_path, output_path):
 
-    image = Image.open(input_path).convert("RGB")
+    with open(input_path, "rb") as input_file:
+        input_data = input_file.read()
 
-    original_size = image.size
+    output_data = remove(
+        input_data,
+        model_name="isnet-general-use"
+    )
 
-    input_tensor = transform_image(image).unsqueeze(0).to(device)
-
-    with torch.no_grad():
-
-        preds = model(input_tensor)[-1].sigmoid().cpu()
-
-    mask = preds[0].squeeze().numpy()
-
-    mask = (mask * 255).astype(np.uint8)
-
-    mask_image = Image.fromarray(mask).resize(original_size)
-
-    image.putalpha(mask_image)
-
-    image.save(output_path)
+    with open(output_path, "wb") as output_file:
+        output_file.write(output_data)
 
     return output_path
+
+
+print("✅ Rembg Ready")
