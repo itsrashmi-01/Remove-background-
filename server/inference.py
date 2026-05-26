44d065e4690ce088a06072c1b1a9948481884cec
I'TS RASHMI <itsrashmi4@gmail.com>
2026-05-26 08:49:47 +0530

    Update inference.py

--- server/inference.py
+++ server/inference.py
@@ -1,26 +1,18 @@
 from rembg import remove
 
-model_loaded = False
-
 
 async def remove_background(input_path, output_path):
 
-    global model_loaded
-
-    if not model_loaded:
-        print("📦 Loading Rembg Model...")
-        model_loaded = True
-        print("✅ Rembg Ready")
+    print("✂️ Processing Image...")
 
     with open(input_path, "rb") as input_file:
         input_data = input_file.read()
 
-    output_data = remove(
-        input_data,
-        model_name="isnet-general-use"
-    )
+    output_data = remove(input_data)
 
     with open(output_path, "wb") as output_file:
         output_file.write(output_data)
+
+    print("✅ Background Removed")
 
     return output_path
