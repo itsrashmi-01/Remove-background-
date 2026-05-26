124e36175642dab15255a2c493dc368232a37d16
I'TS RASHMI <itsrashmi4@gmail.com>
2026-05-26 08:36:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,3 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-HF_TOKEN = os.getenv("HF_TOKEN")
-
-RMBG_MODEL_NAME = "briaai/RMBG-2.0"
